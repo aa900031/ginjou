541f6c99b46acdd8a945b718c8707b2095f304f3
zhong666 <aa900031@gmail.com>
2023-09-18 01:32:34 +0800

    chore: add release-it config

---
+++ internals/release-it-config/index.cjs
@@ -0,0 +1,29 @@
+const CURRENT_PATH = './'
+const VERSION_TEMPLATE = '${version}'
+const PKG_NAME = process.env.npm_package_name
+
+module.exports = {
+	plugins: {
+		'@release-it/conventional-changelog': {
+			path: CURRENT_PATH,
+			preset: 'unjs',
+			infile: 'CHANGELOG.md',
+			header: '# Changelog\n\nAll notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.\n',
+			gitRawCommitsOpts: {
+				path: CURRENT_PATH,
+			},
+		},
+	},
+	git: {
+		push: false,
+		tagName: `${VERSION_TEMPLATE}@${VERSION_TEMPLATE}`,
+		commitsPath: CURRENT_PATH,
+		commitMessage: `chore(release): ${PKG_NAME}v${VERSION_TEMPLATE}`,
+	},
+	github: {
+		release: false,
+	},
+	npm: {
+		publish: false,
+	},
+}
