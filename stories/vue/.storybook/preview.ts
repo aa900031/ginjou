a7a9b4cc612782cc5eb1ceb2ec3c58afe5745573
zhong666 <aa900031@gmail.com>
2024-08-23 16:33:23 +0800

    chore(stroybook-vue): ignore show exception with AbortDefer

--- stories/vue/.storybook/preview.ts
+++ stories/vue/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import 'uno.css'
+import { AbortDefer } from '@ginjou/core'
 import type { Preview } from '@storybook/vue3'
 import { setup } from '@storybook/vue3'
 import PrimeVue from 'primevue/config'
@@ -12,6 +13,18 @@ setup((app) => {
 		},
 	})
 	app.use(ToastService)
+})
+
+setup((app) => {
+	const _originErrorHandler = app.config.errorHandler
+
+	app.config.errorHandler = (err, instance, info) => {
+		// Ignore AbortDefer Exception
+		if (err instanceof AbortDefer)
+			return
+
+		_originErrorHandler?.(err, instance, info)
+	}
 })
 
 export default {
