cb3aa3a0c73feb56c37c834e64aa7f352b600563
zhong666 <aa900031@gmail.com>
2024-10-15 21:43:32 +0800

    test: add useGetList for subscribe

--- packages/vue/src/query/get-list.test.ts
+++ packages/vue/src/query/get-list.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
-import { RealtimeAction, type SubscribeFn } from '@ginjou/core'
+import type { SubscribeFn, UnsubscribeFn } from '@ginjou/core'
+import { RealtimeAction } from '@ginjou/core'
 import { unref } from 'vue'
 import { mountTestApp } from '../../test/mount'
 import { MockFetchers, queryClient } from '../../test/setup'
@@ -42,6 +43,58 @@ describe('useGetList', () => {
 					type: 'list',
 				},
 			})
+		})
+
+		it('should call realtime.unscribe on unmount', async () => {
+			const KEY_FOR_SUB = 'key-for-subscribe'
+			const subscribe = vi.fn<Parameters<SubscribeFn>, ReturnType<SubscribeFn>>(() => KEY_FOR_SUB)
+			const unsubscribe = vi.fn<Parameters<UnsubscribeFn>, ReturnType<UnsubscribeFn>>()
+
+			const { result, unmount } = mountTestApp(
+				() => useGetList({
+					resource: 'posts',
+				}),
+				{
+					queryClient,
+					fetchers: MockFetchers,
+					realtime: {
+						subscribe,
+						unsubscribe,
+					} as any,
+				},
+			)
+
+			await vi.waitFor(() => {
+				expect(unref(result.isFetched)).toBeTruthy()
+			})
+
+			unmount()
+
+			expect(unsubscribe).toBeCalledWith(KEY_FOR_SUB)
+			expect(unsubscribe).toBeCalledTimes(1)
+		})
+
+		it('should not subscribe if queryOptions.enabled is false', async () => {
+			const subscribe = vi.fn<Parameters<SubscribeFn>, ReturnType<SubscribeFn>>()
+
+			const { result } = mountTestApp(
+				() => useGetList({
+					resource: 'posts',
+					queryOptions: {
+						enabled: false,
+					},
+				}),
+				{
+					queryClient,
+					fetchers: MockFetchers,
+					realtime: {
+						subscribe,
+					} as any,
+				},
+			)
+
+			expect(unref(result.isFetched)).toBeFalsy()
+			expect(subscribe).not.toBeCalled()
 		})
 	})
 })
