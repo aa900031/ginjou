7ece20e014a3d6188c4384ae6b9e6d342c7ec70e
zhong666 <aa900031@gmail.com>
2025-06-12 01:36:10 +0800

    chore(nuxt playground): add auth, authz

---
+++ packages/nuxt/playground/ginjou/auth.ts
@@ -0,0 +1,27 @@
+import { defineAuthContext } from '@ginjou/vue'
+
+export default () => defineAuthContext({
+	login: async () => {
+		;(globalThis as any)._AUTH = {
+			name: 'foo',
+		}
+		return {
+			redirectTo: false,
+		}
+	},
+	logout: async () => {
+		delete (globalThis as any)._AUTH
+		return false
+	},
+	getIdentity: () => {
+		return (globalThis as any)._AUTH
+	},
+	check: async () => {
+		return {
+			authenticated: !!(globalThis as any)._AUTH,
+		}
+	},
+	checkError: async () => ({
+		logout: true,
+	}),
+})
