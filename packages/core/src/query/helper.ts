94c282548123e771454c954ee5863240c188a389
zhong666 <aa900031@gmail.com>
2025-06-14 23:52:12 +0800

    fix(core): incorrect update infinite list cache data (#61)

--- packages/core/src/query/helper.ts
+++ packages/core/src/query/helper.ts
@@ -1,3 +1,6 @@
+import type { InfiniteData } from '@tanstack/query-core'
+import type { BaseRecord, RecordKey } from './fetcher'
+
 export async function fakeMany<
 	TData,
 >(
@@ -6,4 +9,41 @@ export async function fakeMany<
 	return {
 		data: (await Promise.all(promises)).map(res => res.data),
 	}
+}
+
+export function isInfiniteData(
+	data: unknown,
+): data is InfiniteData<unknown> {
+	return data != null
+		&& typeof data === 'object'
+		&& 'pages' in data
+		&& 'pageParams' in data
+		&& Array.isArray(data.pages)
+		&& Array.isArray(data.pageParams)
+}
+
+export function checkTargetRecord<
+	T extends BaseRecord,
+>(
+	data: T,
+	ids: RecordKey[],
+): boolean {
+	return (data.id != null)
+		&& !ids.includes(data.id.toString())
+}
+
+export function mergeTargetRecord<
+	T extends BaseRecord,
+>(
+	data: T,
+	ids: RecordKey[],
+	params: any,
+): T {
+	if (data.id != null && ids.includes(data.id.toString())) {
+		return {
+			...data,
+			...params,
+		} as unknown as T
+	}
+	return data
 }
