398b0484edec72000d655a4f50e5cab508cf64e7
zhong666 <aa900031@gmail.com>
2025-11-06 02:17:31 +0800

    fix: vitest config

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -3,42 +3,21 @@ import { isCI } from 'std-env'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+	plugins: isCI
+		? [
+				codspeed(),
+			]
+		: [],
 	test: {
 		dir: './src',
 		coverage: {
 			enabled: true,
 			provider: 'istanbul',
+		},
+		benchmark: {
 			include: [
-				'./src/**/*',
+				'**/*.bench.ts',
 			],
 		},
-		projects: [
-			{
-				plugins: isCI
-					? [
-							codspeed(),
-						]
-					: [],
-				test: {
-					name: 'benchmark',
-					pool: isCI ? 'forks' : undefined,
-					include: [],
-					benchmark: {
-						include: ['src/**/*.bench.ts'],
-					},
-				},
-			},
-			{
-				test: {
-					name: 'unit',
-					include: [
-						'src/**/*.{test,spec}.ts',
-					],
-					benchmark: {
-						include: [],
-					},
-				},
-			},
-		],
 	},
 })
--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
 		coverage: {
 			enabled: true,
 			provider: 'istanbul',
-			include: [
-				'./src/**/*',
-			],
 		},
 	},
 	resolve: {
