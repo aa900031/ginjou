7a0e9d6e2dc77f64b1a53843123b28118bbc9032
zhong666 <aa900031@gmail.com>
2025-11-06 10:30:01 +0800

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
+import { getV8Flags } from '@codspeed/core'
 import codspeed from '@codspeed/vitest-plugin'
+import { isCI } from 'std-env'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
+		poolOptions: isCI
+			? { forks: { execArgv: getV8Flags() } }
+			: undefined,
 		coverage: {
 			enabled: true,
 			provider: 'istanbul',
@@ -14,9 +19,11 @@ export default defineConfig({
 			'packages/core',
 			'packages/vue',
 			{
-				plugins: [
-					codspeed(),
-				],
+				plugins: isCI
+					? [
+							codspeed(),
+						]
+					: [],
 				test: {
 					name: 'benchmark',
 					include: [],
