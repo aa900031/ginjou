6b314d58d7dc0d50f493679a4b667cf59e3107c5
zhong666 <aa900031@gmail.com>
2026-01-01 16:58:09 +0800

    feat(docs): scale up icons

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -13,10 +13,20 @@ export default defineAppConfig({
 			codeIcon: {
 				svelte: 'i-vscode-icons-file-type-svelte',
 			},
+			card: {
+				slots: {
+					icon: 'size-7',
+				},
+			},
 		},
 		contentSearchButton: {
 			slots: {
 				base: 'py-2.5 px-3',
+			},
+		},
+		pageFeature: {
+			slots: {
+				leadingIcon: 'size-6.5',
 			},
 		},
 	},
