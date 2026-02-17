6786f1645e62faa4c297b6e390088782ec1b8515
zhong666 <aa900031@gmail.com>
2026-02-17 22:53:32 +0800

    chore(nuxt): move @nuxt/kit to peer deps

--- packages/nuxt/src/utils/import.ts
+++ packages/nuxt/src/utils/import.ts
@@ -1,5 +1,7 @@
 import type { addImports } from '@nuxt/kit'
 
-export type Import = Parameters<typeof addImports> extends [infer T]
-	? T
+export type Import = Parameters<typeof addImports> extends [infer T, ...any]
+	? T extends Array<infer T2>
+		? T2
+		: T
 	: never
