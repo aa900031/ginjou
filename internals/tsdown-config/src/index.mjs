7f61b50ba90deac9556f0ecffaaf772c8035933a
zhong666 <aa900031@gmail.com>
2026-02-23 20:50:45 +0800

    chore: use @aa900031/tsdown-config

--- internals/tsdown-config/src/index.mjs
+++ internals/tsdown-config/src/index.mjs
@@ -1,4 +1,4 @@
-import * as path from 'node:path'
+import { customExports } from '@aa900031/tsdown-config'
 import { defineConfig } from 'tsdown'
 
 export const DEFAULT_CONFIG = defineConfig({
@@ -6,31 +6,7 @@ export const DEFAULT_CONFIG = defineConfig({
 	clean: true,
 	exports: {
 		devExports: true,
-		customExports(exports,	ctx) {
-			const formats = [['import', 'es'], ['require', 'cjs']]
-			const pkgRoot = path.dirname(ctx.pkg.packageJsonPath)
-
-			for (const [output, config] of Object.entries(exports)) {
-				if (config == null || typeof config !== 'object')
-					continue
-				for (const [name, format] of formats) {
-					const configValue = config[name]
-					const chunks = ctx.chunks[format]
-					if (typeof configValue !== 'string' || chunks == null)
-						continue
-					for (const chunk of chunks) {
-						if (chunk.type === 'chunk' && chunk.name.endsWith('.d')) {
-							exports[output][name] = {
-								default: configValue,
-								types: `./${path.relative(pkgRoot, path.join(chunk.outDir, chunk.fileName))}`,
-							}
-						}
-					}
-				}
-			}
-
-			return exports
-		},
+		customExports,
 	},
 	dts: {
 		compilerOptions: {
