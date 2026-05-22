d65485efa198d8b481cfbfca92c1aa08afaa5a7d
zhong666 <aa900031@gmail.com>
2026-05-22 08:51:30 +0800

    docs: controller replace resource

--- packages/nuxt/playground/app/app.vue
+++ packages/nuxt/playground/app/app.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import defineAuth from '~/ginjou/auth'
 import defineAuthz from '~/ginjou/authz'
+import defineController from '~/ginjou/controller'
 import defineFetcher from '~/ginjou/fetcher'
 import defineQueryClient from '~/ginjou/query'
-import defineResources from '~/ginjou/resource'
 
 defineFetcher()
 defineQueryClient()
-defineResources()
+defineController()
 defineAuth()
 defineAuthz()
 </script>
