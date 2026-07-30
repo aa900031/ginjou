44d05d7300fd38479cd5f21b97156ffcc879b8ed
zhong666 <aa900031@gmail.com>
2026-07-31 00:28:54 +0800

    refactor(adapters): sort barrel exports (#152)

--- packages/vue/src/query/index.ts
+++ packages/vue/src/query/index.ts
@@ -1,20 +1,15 @@
 export * from './create'
 export * from './create-many'
-
 export * from './custom'
 export * from './custom-mutation'
 export * from './delete'
 export * from './delete-many'
-
 export * from './fetchers'
 export * from './get-infinite-list'
-
 export * from './get-list'
 export * from './get-many'
 export * from './get-many-by-one'
-
 export * from './get-one'
 export * from './query-client'
-
 export * from './update'
 export * from './update-many'
